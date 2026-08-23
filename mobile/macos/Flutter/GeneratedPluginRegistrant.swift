cc58717febbcafd8b5b4ead6ab00794a3e76c15c
philmorehost <philmorehost@gmail.com>
2026-08-23 22:20:12 +0100

    chore(mobile): regenerate desktop plugin registrants for image_picker

--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -12,6 +13,7 @@ import video_player_avfoundation
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
